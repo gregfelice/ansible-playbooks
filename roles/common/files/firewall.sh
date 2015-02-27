3c50da18d892184a0c75fb9ea6a06294551b4a58
Greg <gregf@byrd.constantlabs.com>
2015-02-27 09:39:00 -0500

    minor changes

--- roles/common/files/firewall.sh
+++ roles/common/files/firewall.sh
@@ -5,3 +5,4 @@
 /usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -p tcp -m tcp --dport 22 -m recent --update --seconds 180 --hitcount 3 --rttl --name SSH --rsource -m comment --comment "SSH Brute-force protection" -j DROP
 
 /usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 2 -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name SSH --rsource -m comment --comment "SSH Brute-force protection" -j ACCEPT
+
