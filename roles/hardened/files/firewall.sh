719db2d516abe52f9bb2953e81f6f2d67107a882
Greg Felice <gregfelice@gmail.com>
2015-03-06 14:25:15 -0500

    added stage and production list examples, expunged actual lists got bootstrap, hardening, and common roles passing
    still does not work to include actual master machine.. need to address this

--- roles/hardened/files/firewall.sh
+++ roles/hardened/files/firewall.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-/usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp -m tcp --dport 22 -m recent --update --seconds 180 --hitcount 3 --rttl --name SSH --rsource -m comment --comment "SSH Brute-force protection" -j LOG --log-prefix "SSH_brute_force " 
+DPORT=22
+SECONDS=180
+HITCOUNT=3
+COMMENT="SSH Brute-force protection"
+LOG_PREFIX="SSH_brute_force "
 
-/usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -p tcp -m tcp --dport 22 -m recent --update --seconds 180 --hitcount 3 --rttl --name SSH --rsource -m comment --comment "SSH Brute-force protection" -j DROP
+/usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp -m tcp --dport $DPORT -m recent --update --seconds $SECONDS --hitcount $HITCOUNT --rttl --name SSH --rsource -m comment --comment $COMMENT -j LOG --log-prefix $LOG_PREFIX
 
-/usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 2 -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name SSH --rsource -m comment --comment "SSH Brute-force protection" -j ACCEPT
+/usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -p tcp -m tcp --dport $DPORT -m recent --update --seconds $SECONDS --hitcount $HITCOUNT --rttl --name SSH --rsource -m comment --comment $COMMENT -j DROP
 
+/usr/bin/firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 2 -p tcp -m tcp --dport $DPORT -m state --state NEW -m recent --set --name SSH --rsource -m comment --comment $COMMENT -j ACCEPT
